2c88b8f40af049dd731a031fe6bfb34995baf556
Joki <josedavidamayar@gmail.com>
2025-03-19 23:52:19 -0500

    add a lsp config for the tsserver and deno

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,30 @@ return {
   opts = {
     servers = {
       tailwindcss = {},
+      denols = {
+        root_dir = function(fname)
+          local deno_root = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc")(fname)
+          local node_root = require("lspconfig").util.root_pattern("package.json")(fname)
+
+          if deno_root and not node_root then
+            return deno_root
+          end
+        end,
+        init_options = {
+          lint = true,
+          unstable = true,
+        },
+      },
+      tsserver = {
+        root_dir = function(fname)
+          local node_root = require("lspconfig").util.root_pattern("package.json")(fname)
+          local deno_root = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc")(fname)
+
+          if node_root and not deno_root then
+            return node_root
+          end
+        end,
+      },
     },
   },
 }
