09e74e787d2f915c4f891174196a5fa028173273
Joki <josedavidamayar@gmail.com>
2025-03-28 02:39:37 -0500

    change to rose theme and update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,17 @@
+-- return {
+--   { "catppuccin/nvim", name = "catppuccin", lazy = false, opts = { flavour = "mocha" } },
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = "catppuccin",
+--    },
+--  },
+-- }
+
 return {
-  { "catppuccin/nvim", name = "catppuccin", lazy = false, opts = { flavour = "mocha" } },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+  "rose-pine/neovim",
+  name = "rose-pine",
+  config = function()
+    vim.cmd("colorscheme rose-pine")
+  end,
 }
