a273cb55679281a98637736d197f51bb9af46fce
Joki <josedavidamayar@gmail.com>
2025-03-29 09:13:06 -0500

    feat: add the lsp cpp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,15 @@ return {
   opts = {
     servers = {
       tailwindcss = {},
+      clangd = { -- C++ LSP
+        cmd = { "clangd", "--background-index", "--clang-tidy" },
+        filetypes = { "c", "cpp", "objc", "objcpp" },
+        root_dir = function(fname)
+          return require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt", ".git")(fname)
+            or vim.fn.getcwd()
+        end,
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      },
     },
   },
 }
