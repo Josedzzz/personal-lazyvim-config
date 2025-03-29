0fe144f23e917fbf93e097cc56fc561891e236d0
Joki <josedavidamayar@gmail.com>
2025-03-29 09:05:02 -0500

    feat: add the catppuccin theme transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,14 +8,40 @@
 --  },
 -- }
 
+-- return {
+--  "rose-pine/neovim",
+--  name = "rose-pine",
+--  config = function()
+--    require("rose-pine").setup({
+--      disable_background = true, -- Enables transparency
+--      disable_float_background = true, -- Transparent floating windows
+--    })
+--    vim.cmd("colorscheme rose-pine")
+--  end,
+-- }
+
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    require("rose-pine").setup({
-      disable_background = true, -- Enables transparency
-      disable_float_background = true, -- Transparent floating windows
-    })
-    vim.cmd("colorscheme rose-pine")
-  end,
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    opts = {
+      flavour = "mocha", -- Options: latte, frappe, macchiato, mocha
+      transparent_background = true, -- Enables transparency
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        notify = false,
+        mini = false,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
 }
