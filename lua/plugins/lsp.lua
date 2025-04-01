e6e39e102a8546bc0ad3e2460556c105b1864549
Joki <josedavidamayar@gmail.com>
2025-04-01 09:54:07 -0500

    feat: add the prettier format

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,6 +22,31 @@ return {
           clangdFileStatus = true,
         },
       },
+      pyright = {},
+      denols = {
+        root_dir = function(fname)
+          return require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")(fname)
+        end,
+      },
+      -- -- Use vtsls ONLY in non-Deno projects
+      -- vtsls = {
+      --   root_dir = function(fname)
+      --     local util = require("lspconfig.util")
+      --     return util.root_pattern("package.json")(fname) and not util.root_pattern("deno.json", "deno.jsonc")(fname)
+      --   end,
+      --   settings = {
+      --     vtsls = {
+      --       autoUseWorkspaceTsdk = true,
+      --       enableMoveToFileCodeAction = true,
+      --       experimental = {
+      --         completion = {
+      --           enableServerSideFuzzyMatch = true,
+      --         },
+      --         maxInlayHintLength = 30,
+      --       },
+      --     },
+      --   },
+      -- },
     },
   },
 }
